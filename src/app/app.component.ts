3f3e5b776ec26fd2ec74d9339f9bf3a0f995378d
teocchiii <u202211212@upc.edu.pe>
2024-09-25 00:26:46 -0500

    chore: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCard, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardSubtitle, MatCardTitle, MatCard, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
