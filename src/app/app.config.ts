49e3e8846c7c95aabecb7006183a0e9647a3fcc9
teocchiii <u202211212@upc.edu.pe>
2024-11-21 17:19:05 -0500

    feat(i18n): added internationalization to the component.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+export function HttpLoaderFactory(http: HttpClient):TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })),
+    provideAnimationsAsync('noop')]
 };
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import {LanguageSwitcherComponent} from "../../components/language-switcher/language-switcher.component";
 import {MatCard, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 import {MatToolbar} from "@angular/material/toolbar";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-    imports: [
-        LanguageSwitcherComponent,
-        MatCard,
-        MatCardSubtitle,
-        MatCardTitle,
-        MatToolbar
-    ],
+  imports: [
+    LanguageSwitcherComponent,
+    MatCard,
+    MatCardSubtitle,
+    MatCardTitle,
+    MatToolbar,
+    TranslateModule
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
